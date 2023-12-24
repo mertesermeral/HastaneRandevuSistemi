40c353f758d125978a9befd0c5bdf54e49ec89ed
Mert Eser Meral <eser.meral@ogr.sakarya.edu.tr>
2023-12-25 00:41:29 +0300

    controller calismalari

---
+++ HastaneRandevuSistemi/Controllers/DoktorAdminController.cs
@@ -0,0 +1,107 @@
+﻿using HastaneRandevuSistemi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PagedList;
+namespace HastaneRandevuSistemi.Controllers
+{
+    public class DoktorAdminController : Controller
+    {
+        HastaneContext db = new HastaneContext();
+        public ActionResult Index(string ara, int sayfa = 1)
+        {
+            List<Doktor> degerler = db.Doktor.ToList();
+            return View(db.Doktor.Where(s => s.DoktorAd.ToLower().Contains(ara) || ara == null).ToList().ToPagedList(sayfa, 15));
+        }
+        Class1 cs = new Class1();
+        public ActionResult Ekleme()
+        {
+            List<SelectListItem> firmas = (from x in db.Sehirler.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.SehirAd,
+                                               Value = x.SehirlerID.ToString(),
+                                           }).ToList();
+            ViewBag.frms = firmas;
+
+            List<SelectListItem> degerler = (from i in db.Hastane.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.HastaneAd,
+                                                 Value = i.HastaneID.ToString(),
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            List<SelectListItem> marks = (from n in db.Poliklinik.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = n.PolAd,
+                                              Value = n.PolID.ToString(),
+                                          }).ToList();
+            ViewBag.mrklr = marks;
+            ViewBag.HastaList = new SelectList(hastaneget(), "HastaneID", "HastaneAd");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekleme(Doktor p1)
+        {
+            Doktor ab = new Doktor();
+            ab.HastaneID = p1.HastaneID;
+            ab.PolID = p1.PolID;
+            ab.DoktorAd = p1.DoktorAd;
+
+            db.Doktor.Add(ab);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var urnler = db.Doktor.Find(id);
+            db.Doktor.Remove(urnler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Güncelle(int id)
+        {
+            var ur = db.Doktor.Find(id);
+            List<SelectListItem> degerler = (from i in db.Hastane.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.HastaneAd,
+                                                 Value = i.HastaneID.ToString(),
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            List<SelectListItem> marks = (from n in db.Poliklinik.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = n.PolAd,
+                                              Value = n.PolID.ToString(),
+                                          }).ToList();
+            ViewBag.mrklr = marks;
+            return View("Güncelle", ur);
+        }
+        public ActionResult Guncel(Doktor p1)
+        {
+            var u = db.Doktor.Find(p1.DoktorID);
+            var fr = db.Hastane.Where(m => m.HastaneID == p1.Hastane.HastaneID).FirstOrDefault();
+            u.HastaneID = fr.HastaneID;
+            var mrk = db.Poliklinik.Where(m => m.PolID == p1.Pol.PolID).FirstOrDefault();
+            u.PolID = mrk.PolID;
+            u.DoktorAd = p1.DoktorAd;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public List<Hastaneler> hastaneget()
+        {
+            List<Hastaneler> hastaneler = db.Hastane.ToList();
+
+            return hastaneler;
+        }
+
+
+        public ActionResult klinikgetir(int HASTANEID)
+        {
+            List<Poliklinik> selectlist = db.Poliklinik.Where(x => x.HastaneID == HASTANEID).ToList();
+            ViewBag.Kliniklist = new SelectList(selectlist, "PolID", "PolAd");
+            return PartialView("klinikgoster");
+        }
+    }
+}
--- HastaneRandevuSistemi/Controllers/LoginController.cs
+++ HastaneRandevuSistemi/Controllers/LoginController.cs
@@ -1,12 +1,90 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HastaneRandevuSistemi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HastaneRandevuSistemi.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        // GET: Login
+        HastaneContext db = new HastaneContext();
+        public ActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Kullanici U)
+        {
+            if (ModelState.IsValid)
+            {
+                using (HastaneContext dc = new HastaneContext())
+                {
+                    if (dc.Kullanici.Any(x => x.KullaniciTC == U.KullaniciTC))
+                    {
+                        ViewBag.DuplicateMessage = "Kullanıcı zaten mevcut";
+                        return View(U);
+                    }
+                    U.AilehID = 2074;
+                    dc.Kullanici.Add(U);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                    U = null;
+                    ViewBag.Message = "Kaydınız başarıyla tamamlanmıştır";
+                }
+            }
+            return View(U);
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Kullanici US)
+        {
+            var checkLogin = db.Kullanici.Where(x => x.KullaniciTC.Equals(US.KullaniciTC) && x.KullaniciSifre.Equals(US.KullaniciSifre)).FirstOrDefault();
+            if (checkLogin != null)
+            {
+                HttpContext.Session.SetString("KullaniciTCSS", US.KullaniciTC.ToString());
+                //Session["KullaniciTCSS"] = US.KullaniciTC.ToString();
+                TempData["UserName"] = checkLogin.KullaniciAd;
+                FormsAuthentication.SetAuthCookie(US.KullaniciTC, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Yanlış TC veya şifre";
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult PersonelG()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PersonelG(Personel PS)
+        {
+            var checkLogin = db.Personel.Where(x => x.PUsername.Equals(PS.PUsername) && x.PPasword.Equals(PS.PPasword)).FirstOrDefault();
+            if (checkLogin != null)
+            {
+                HttpContext.Session.SetString("PUsernameSS", PS.PUsername.ToString());
+                //Session["PUsernameSS"] = PS.PUsername.ToString();
+
+                return RedirectToAction("Index", "AdminP");
+            }
+            else
+            {
+                ViewBag.Notification = "Yanlış kullanıcı adı veya şifre";
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Login");
         }
     }
 }
